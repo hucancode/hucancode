8308e99dc54b2016dbf32d771ed9c1c2e02ad9e4
hucancode <hucancode@gmail.com>
2021-08-09 02:31:33 +0900

    extra threejs class

--- src/components/saborScene.js
+++ src/components/saborScene.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import * as THREE from "three";
+import { FBXLoader } from "../three/loaders/FBXLoader";
+import { OrbitControls } from "../three/controls/OrbitControls";
 
 const Container = styled.div`
     min-height: 300px;
@@ -24,25 +26,30 @@ const Canvas = styled.canvas`
 
 class SceneComponent extends Component {
     componentDidMount() {
-      let canvas = document.getElementById('renderer');
-      let w = window.innerWidth*0.8;
-      let h = window.innerHeight*0.8;
-      var renderer = new THREE.WebGLRenderer({canvas: canvas});
-      renderer.setSize( w, h );
-      
-      var scene = new THREE.Scene();
-      var camera = new THREE.PerspectiveCamera( 75, w/h, 0.1, 1000 );
-      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-      var material = new THREE.MeshBasicMaterial( { color: 0x61dafb } );
-      var cube = new THREE.Mesh( geometry, material );
-      scene.add( cube );
-      camera.position.z = 5;
+      let camera, scene, renderer, cube;
+      const clock = new THREE.Clock();
+      let mixer;
+      var init = function () {
+        let canvas = document.getElementById('renderer');
+        let w = window.innerWidth*0.8;
+        let h = window.innerHeight*0.8;
+        renderer = new THREE.WebGLRenderer({canvas: canvas});
+        renderer.setSize( w, h );
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, w/h, 0.1, 1000 );
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x61dafb } );
+        cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+        camera.position.z = 5;
+      }
       var animate = function () {
         requestAnimationFrame( animate );
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
         renderer.render( scene, camera );
       };
+      init();
       animate();
     }
     render() {
