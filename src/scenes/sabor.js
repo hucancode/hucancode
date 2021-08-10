26b7d065256c6d8d5cdb9af84c901af04f70f7b4
hucancode <hucancode@gmail.com>
2021-08-10 09:33:59 +0900

    code format

--- src/scenes/sabor.js
+++ src/scenes/sabor.js
@@ -2,70 +2,61 @@ import * as THREE from "three";
 import { FBXLoader } from "../three/loaders/FBXLoader";
 import { OrbitControls } from "../three/controls/OrbitControls";
 
-let camera, scene, renderer;
+let camera, scene, renderer, animator;
 const clock = new THREE.Clock();
-let mixer;
+const USE_CAMERA_CONTROL = false
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
 export function init() {
     let canvas = document.getElementById('renderer');
-    let w = window.innerWidth*0.8;
-    let h = window.innerHeight*0.8;
-    
-    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-    camera.position.set( 400, 220, 400 );
-    camera.lookAt(0,100,0);
+    let w = window.innerWidth * 0.8;
+    let h = window.innerHeight * 0.8;
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+    camera.position.set(400, 220, 400);
+    camera.lookAt(0, 100, 0);
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0x282c34 );
-    scene.fog = new THREE.Fog( 0xa0a0a0, 100, 2000 );
+    scene.background = new THREE.Color(0x282c34);
+    //scene.fog = new THREE.Fog(0xa0a0a0, 100, 2000);
 
-    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-    hemiLight.position.set( 0, 200, 0 );
-    scene.add( hemiLight );
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    hemiLight.position.set(0, 200, 0);
+    scene.add(hemiLight);
 
-    const dirLight = new THREE.DirectionalLight( 0xffffff );
-    dirLight.position.set( 0, 200, 100 );
+    const dirLight = new THREE.DirectionalLight(0xffffff);
+    dirLight.position.set(0, 200, 100);
     dirLight.castShadow = true;
     dirLight.shadow.camera.top = 180;
     dirLight.shadow.camera.bottom = - 100;
     dirLight.shadow.camera.left = - 120;
     dirLight.shadow.camera.right = 120;
-    scene.add( dirLight );
+    scene.add(dirLight);
 
     // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
 
     // ground
-    const ground = new THREE.Mesh( 
-        new THREE.CircleGeometry( 200, 50, 0, Math.PI*2), 
-        new THREE.MeshPhongMaterial( { color: 0x282c34, depthWrite: false } ) );
+    const ground = new THREE.Mesh(
+        new THREE.CircleGeometry(200, 50, 0, Math.PI * 2),
+        new THREE.MeshPhongMaterial({ color: 0x282c34, depthWrite: false }));
     ground.rotation.x = - Math.PI / 2;
     ground.receiveShadow = true;
     ground.material.opacity = 0.2;
     ground.material.transparent = true;
     scene.add(ground);
 
-    //const grid = new THREE.GridHelper( 200, 10, 0x000000, 0x000000 );
-    //grid.material.opacity = 0.2;
-    //grid.material.transparent = true;
-    //scene.add( grid );
-
     // model
-    
     const loader = new FBXLoader();
     loader.setPath('assets/fbx/');
     loader.setResourcePath('assets/textures/');
-    loader.load( 'SarborV2.fbx', function ( object ) {
-        mixer = new THREE.AnimationMixer( object );
-        object.animations.forEach((e) => {
-            console.log(e.name);
-        });
-        const action = mixer.clipAction(object.animations.find(e => e.name === 'idle'));
+    loader.load('SarborV2.fbx', function (object) {
+        animator = new THREE.AnimationMixer(object);
+        const action = animator.clipAction(object.animations.find(e => e.name === 'idle'));
         action.play();
         object.traverse(child => {
             if (!child.isMesh) {
@@ -74,24 +65,28 @@ export function init() {
             child.castShadow = true;
             child.receiveShadow = false;
             child.material.vertexColors = false;
-            child.material.shininess = child.material.name==='body'?1.0:10.0;
+            child.material.shininess = child.material.name === 'body' ? 1.0 : 10.0;
         });
         scene.add(object);
-    } );
+    });
 
-    renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( w, h );
+    renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(w, h);
     renderer.shadowMap.enabled = true;
-    // const controls = new OrbitControls( camera, renderer.domElement );
-    // controls.target.set( 0, 100, 0 );
-    // controls.update();
-    window.addEventListener( 'resize', onWindowResize );
+    if (USE_CAMERA_CONTROL) {
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.target.set(0, 100, 0);
+        controls.update();
+    }
+    window.addEventListener('resize', onWindowResize);
 }
 
 export function animate() {
-    requestAnimationFrame( animate );
+    requestAnimationFrame(animate);
     const delta = clock.getDelta();
-    if ( mixer ) mixer.update( delta );
-    renderer.render( scene, camera );
+    if (animator) {
+        animator.update(delta);
+    }
+    renderer.render(scene, camera);
 }
