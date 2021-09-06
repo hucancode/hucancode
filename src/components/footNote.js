7b798385efa4d7c2a01783123235a65fea8df8d3
hucancode <hucancode@gmail.com>
2021-09-06 18:48:13 +0900

    fix foot note translation

--- src/components/footNote.js
+++ src/components/footNote.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { Trans } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
 const Container = styled.div`
     ${tw`
@@ -23,12 +23,15 @@ const Container = styled.div`
 
 
 export default function FootNote() {
+    useTranslation();
     return <Container>
         <p>
-            <Trans  i18nKey="note.madeWith">Made with <code>React.js</code>, <code>Three.js</code> and 10+ other tools<br/></Trans>
-            <Trans  i18nKey="note.assetCredit">
-            <small>Dragon model by <a href="https://sketchfab.com/3d-models/chinese-dragon-fa05f2a6596041938152a84a956212e0">youmeowmeow</a></small>. 
-            <small>Spider model by <a href="https://sketchfab.com/3d-models/low-poly-spider-walk-cycle-e4d2c40b66554b10be20f61bb0610774">volkanongun</a></small>
+            <Trans i18nKey="note.madeWith">
+                Made with <code>React.js</code>, <code>Three.js</code> and 10+ other tools<br/>
+            </Trans>
+            <Trans i18nKey="note.assetCredit">
+                <small>Dragon model by <a href="https://sketchfab.com/3d-models/chinese-dragon-fa05f2a6596041938152a84a956212e0">youmeowmeow</a></small>. 
+                <small>Spider model by <a href="https://sketchfab.com/3d-models/low-poly-spider-walk-cycle-e4d2c40b66554b10be20f61bb0610774">volkanongun</a></small>
             </Trans>
         </p>
         
