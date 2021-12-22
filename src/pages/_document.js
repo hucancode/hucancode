2cfcd36b0af9e51d1bd8d9b83c4dd1bed900d4e3
hucancode <hucancode@gmail.com>
2021-12-22 11:01:53 +0900

    add page title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,7 +29,10 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+            <title>hucancode</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <body>
           <Main />
           <NextScript />
