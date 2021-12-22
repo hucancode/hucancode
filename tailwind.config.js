2b657d61cc0675bbf65592d4a13de3cf0a6f2df6
hucancode <hucancode@gmail.com>
2021-12-23 00:30:48 +0900

    minor code format

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,44 +7,43 @@ module.exports = {
     ],
     darkMode: 'class',
     theme: {
-      extend: {
-        zIndex: {
-          '-10': '-10',
-          '10': '10',
+        extend: {
+            zIndex: {
+                '-10': '-10',
+                '10': '10',
+            },
+            skew: {
+                '20': '20deg',
+                '-20': '-20deg',
+                '45': '45deg',
+                '60': '60deg',
+                '80': '80deg',
+                '-80': '-80deg',
+            },
+            width: {
+                '3/2': '150%',
+                '5/2': '250%',
+            },
         },
-        skew: {
-          '20': '20deg',
-          '-20': '-20deg',
-          '45': '45deg',
-          '60': '60deg',
-          '80': '80deg',
-          '-80': '-80deg',
-         },
-         width: {
-          '3/2': '150%',
-          '5/2': '250%',
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
         },
-      },
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
-      },
-      container: {
-        padding: {
-          DEFAULT: '1rem',
-          sm: '2rem',
-          lg: '4rem',
-          xl: '5rem',
-          '2xl': '6rem',
+        container: {
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+            },
         },
-      },
     },
     variants: {
-      extend: {},
+        extend: {},
     },
     plugins: [],
-  }
-  
+}
