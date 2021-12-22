0e915e140c74e35822d03e44274d895d28a664cc
hucancode <hucancode@gmail.com>
2021-12-23 00:02:08 +0900

    fix warning

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { appWithTranslation } from 'next-i18next';
 import '../styles/index.css';
+import Head from 'next/head'
 
 function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return <>
+        <Head>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
 
 export default appWithTranslation(App);
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -29,9 +29,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
+        <Head/>
         <body>
           <Main />
           <NextScript />
