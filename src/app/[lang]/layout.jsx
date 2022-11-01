fa44dd57e28ad16aaac60a21c3a19a1b863c0b3c
SatoshiKIYOTA <hucancode@gmail.com>
2022-11-01 17:14:04 +0900

    move language loading logic to root layout

--- src/app/[lang]/layout.jsx
+++ src/app/[lang]/layout.jsx
@@ -1,15 +1,23 @@
-"use client";
 import React from "react";
 import Navbar from "components/navigation-bar";
 import FootNote from "components/foot-note";
 import I18n from "locales/i18n";
 import "styles/global.css";
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ params, children }) {
+  const { lang } = params;
+  let dictionary = null;
+  if (lang === "jp") {
+    dictionary = (await import("locales/jp.json")).default;
+    console.log("set language to JP");
+  } else {
+    dictionary = (await import("locales/en.json")).default;
+    console.log("set language to EN");
+  }
   return (
     <html>
       <body className="page-container">
-        <I18n>
+        <I18n lngDict={dictionary} locale={lang}>
           <Navbar />
           <main>{children}</main>
           <FootNote />
--- src/app/[lang]/page.jsx
+++ src/app/[lang]/page.jsx
@@ -1,31 +1,13 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import LandingSection from "components/landing-section";
 import SkillSection from "components/skill-section";
 import LanguageSection from "components/language-section";
 import ExperienceSection from "components/experience-section";
 import ChallengeSection from "components/challenge-section";
 import FooterSection from "components/footer-section";
-import useI18n from "locales/use-i18n";
 
-async function setLanguage(lang, i18n) {
-  if (lang === "jp") {
-    const JP = await import("locales/jp.json");
-    i18n.locale("jp", JP);
-    console.log("set language to JP");
-  } else {
-    const EN = await import("locales/en.json");
-    i18n.locale("en", EN);
-    console.log("set language to EN");
-  }
-}
-export default function Home({ params }) {
-  const { lang } = params;
-  const i18n = useI18n();
-  useEffect(() => {
-    setLanguage(lang, i18n);
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+export default function Home() {
   return (
     <>
       <LandingSection />
