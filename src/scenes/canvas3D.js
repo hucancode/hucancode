31e248323ebaa7003a43c3e5154b675cffdac73b
hucancode <hucancode@gmail.com>
2022-11-06 18:26:58 +0900

    add loading indicator for canvas3D

--- src/scenes/canvas3D.js
+++ src/scenes/canvas3D.js
@@ -6,6 +6,7 @@ export default class Canvas3D extends React.Component {
     super(props);
     this.state = { isInCamera: false };
     this.canvasRef = React.createRef();
+    this.loadingRef = React.createRef();
     this.frameID = 0;
   }
   componentDidMount() {
@@ -15,6 +16,9 @@ export default class Canvas3D extends React.Component {
     });
     if (this.canvasRef.current) {
       this.observer.observe(this.canvasRef.current);
+    }
+    if (this.loadingRef.current) {
+      this.loadingRef.current.classList.add("hidden");
     }
   }
   componentWillUnmount() {
@@ -34,7 +38,16 @@ export default class Canvas3D extends React.Component {
   }
   render() {
     return (
-      <canvas className="w-full" id={this.canvasID} ref={this.canvasRef} />
+      <div className="w-full h-full relative">
+        <div ref={this.loadingRef} className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
+          <svg className="animate-spin h-10 w-10 dark:text-white text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        </div>
+        <canvas className="w-full" id={this.canvasID} ref={this.canvasRef}>
+      </canvas>
+      </div>
     );
   }
 }
