95e3e3d7cec47148fe35ad72864df4eefab00e3e
SatoshiKIYOTA <hucancode@gmail.com>
2022-12-08 16:29:19 +0900

    small fix

--- src/widgets/dual-tag.jsx
+++ src/widgets/dual-tag.jsx
@@ -5,7 +5,7 @@ export default function DualTag(props) {
   let value = props.value;
   return (
     <div className="flex flex-col md:flex-row">
-      <span className="bg-sky-200 px-2 py-1 pr-4 text-gray-700 dark:bg-gray-700 dark:text-gray-100">
+      <span className="bg-sky-200 px-2 py-1 text-gray-700 dark:bg-gray-700 dark:text-gray-100">
         {title}
       </span>
       <span className="bg-gray-800 px-2 py-1 text-gray-400 dark:bg-white dark:text-gray-700">
