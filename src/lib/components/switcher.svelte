d0266cead017d7191c1a867f88b013ce34ed0a7b
hucancode <hucancode@gmail.com>
2022-12-16 20:48:13 +0900

    small adjust

--- src/lib/components/switcher.svelte
+++ src/lib/components/switcher.svelte
@@ -24,61 +24,47 @@
   }
 </script>
 
-<div class="outline-gray-900/10 dark:outline-white/10">
+<div
+  class="relative flex max-w-lg items-center justify-center overflow-hidden outline outline-1 outline-gray-900/10 dark:outline-white/10"
+>
   <input
     {id}
     type="checkbox"
-    class="peer"
+    class="peer hidden"
     checked={value}
     on:change={switchAB}
   />
   <button
-    class="text-red-600 peer-checked:text-gray-700 dark:peer-checked:text-gray-400"
+    class="z-10 w-1/2 cursor-pointer
+			select-none
+			px-4
+			py-2
+			text-xs
+			text-red-600 duration-300 peer-checked:text-gray-700 dark:peer-checked:text-gray-400 md:text-base"
     on:click={selectA}
   >
     <slot name="label-a">Label A</slot>
   </button>
   <label
     for={id}
-    class="peer-checked:-translate-x-1/3
-		dark:bg-gray-700"
+    class="absolute left-0 flex h-full
+      w-[150%]
+			cursor-pointer
+			justify-between
+			rounded-2xl bg-sky-200 duration-100 peer-checked:-translate-x-1/3 dark:bg-gray-700"
   >
-    <span />
-    <span />
+    <span class="h-full w-1/3 bg-black" />
+    <span class="h-full w-1/3 bg-black" />
   </label>
   <button
-    class="text-gray-700 peer-checked:text-red-600 dark:text-gray-400"
+    class="z-10 w-1/2 cursor-pointer
+			select-none
+			px-4
+			py-2
+			text-xs
+			text-gray-700 duration-300 peer-checked:text-red-600 dark:text-gray-400 md:text-base"
     on:click={selectB}
   >
     <slot name="label-b">Label B</slot>
   </button>
 </div>
-
-<style lang="postcss">
-  div {
-    @apply relative flex max-w-lg items-center justify-center overflow-hidden outline outline-1;
-  }
-  input {
-    @apply hidden;
-  }
-  label {
-    @apply absolute left-0
-      flex
-			h-full
-			w-[150%]
-			cursor-pointer justify-between rounded-2xl
-        bg-sky-200 duration-100;
-  }
-  span {
-    @apply h-full w-1/3 bg-black;
-  }
-  button {
-    @apply z-10
-  w-1/2 cursor-pointer
-			select-none
-			px-4
-			py-2
-			text-xs
-			duration-300 md:text-base;
-  }
-</style>
