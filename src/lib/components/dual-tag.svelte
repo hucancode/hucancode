e6d6c6686c9e453c56364e99dd72b999689e37eb
hucancode <hucancode@gmail.com>
2022-12-18 09:48:20 +0900

    small adjust

--- src/lib/components/dual-tag.svelte
+++ src/lib/components/dual-tag.svelte
@@ -5,12 +5,12 @@
 
 <div class="flex flex-col md:flex-row">
   <span
-    class="bg-sky-200 px-2 py-1 text-gray-700 dark:bg-gray-700 dark:text-gray-100"
+    class="bg-sky-200 px-2 py-1 text-center text-gray-700 dark:bg-gray-700 dark:text-gray-100"
   >
     {title}
   </span>
   <span
-    class="bg-gray-800 px-2 py-1 text-gray-400 dark:bg-white dark:text-gray-700"
+    class="bg-gray-800 px-2 py-1 text-center text-gray-400 dark:bg-white dark:text-gray-700"
   >
     {value}
   </span>
