2ebfd57b2f11ab23745631a1ad62441d1d08d519
hucancode <hucancode@gmail.com>
2022-12-18 20:40:54 +0900

    small adjust

--- src/lib/components/leetcode-chart.svelte
+++ src/lib/components/leetcode-chart.svelte
@@ -102,7 +102,7 @@
   });
 </script>
 
-<div class="w-full lg:w-1/2 p-5">
+<div class="w-full lg:w-1/2 max-w-screen-sm p-5">
   <div class="mb-4 flex justify-center gap-2">
     <div class="flex flex-col items-center justify-center gap-2 md:flex-row">
       <Leetcode class="text-lg" />
