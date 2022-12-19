c2886dd8908bb45c3545053c60e8cde27bb4389b
hucancode <hucancode@gmail.com>
2022-12-19 09:16:55 +0900

    remove all analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,5 @@
 <script>
   import "$lib/styles/app.css";
-  import { webVitals } from '$lib/vitals';
-  import { inject } from '@vercel/analytics';
-  import { browser } from '$app/environment';
-  import { page } from '$app/stores';
-
-  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-  $: if (browser && analyticsId) {
-    inject();
-    webVitals({
-      path: $page.url.pathname,
-      params: $page.params,
-      analyticsId
-    })
-  }
 </script>
 
 <svelte:head>
