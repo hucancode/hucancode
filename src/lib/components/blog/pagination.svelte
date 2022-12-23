2231ab04845ca3ec377a0fb9ba040ab9084651d5
hucancode <hucancode@gmail.com>
2022-12-24 00:53:42 +0900

    small adjust

--- src/lib/components/blog/pagination.svelte
+++ src/lib/components/blog/pagination.svelte
@@ -24,7 +24,9 @@
   {#if lastPage > 1}
     <ul class="flex gap-4 text-sm font-bold">
       {#each pages as p}
-        <li class="aspect-square w-12 p-2 dark:bg-gray-700 dark:text-gray-200">
+        <li
+          class="aspect-square w-12 bg-sky-200 p-2 text-gray-600 dark:bg-gray-700 dark:text-gray-200"
+        >
           <a
             sveltekit:noscroll
             class:pointer-events-none={p == page}
