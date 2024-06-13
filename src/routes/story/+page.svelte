f3d5849cc656faed9b5feae5aa9a4c1ce0dc5623
hucancode <hucancode@gmail.com>
2024-06-13 20:04:37 +0900

    fix typo

--- src/routes/story/+page.svelte
+++ src/routes/story/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import Nav from "$lib/components/nav.svelte";
   import Greetings from "$lib/components/story/greetings.svelte";
-  import Showcase from "$lib/components/story/Showcase.svelte";
+  import Showcase from "$lib/components/story/showcase.svelte";
 </script>
 
 <svelte:head>
