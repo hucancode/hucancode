74d1087fae7df0ff35aa9ae0d5570eb3390576d0
hucancode <hucancode@gmail.com>
2025-01-09 23:46:25 +0900

    rustup now install without prompts

--- .vercel/install.sh
+++ .vercel/install.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | bash -s -- -y
 source $HOME/.cargo/env
 rustup target add wasm32-unknown-unknown
 cargo install wasm-bindgen-cli
