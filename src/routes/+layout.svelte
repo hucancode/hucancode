135abdf232db6514a3d19a90ec265e449b22b2a4
hucancode <hucancode@gmail.com>
2025-01-12 20:05:34 +0900

    optimize font stack

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,12 +21,6 @@
 </script>
 
 <svelte:head>
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
-    rel="stylesheet"
-  />
   <script
     async
     src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
