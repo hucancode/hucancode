78cf1200616ac7ba2f6b45d4c809bbcb312003c8
hucancode <hucancode@gmail.com>
2025-07-24 22:35:51 +0900

    fix go kotlin syntax hightlight

--- src/lib/markdoc/renderers/CodeRenderer.svelte
+++ src/lib/markdoc/renderers/CodeRenderer.svelte
@@ -8,6 +8,8 @@
   import 'prismjs/components/prism-css';
   import 'prismjs/components/prism-python';
   import 'prismjs/components/prism-rust';
+  import 'prismjs/components/prism-go';
+  import 'prismjs/components/prism-kotlin';
   import 'prismjs/components/prism-bash';
   import 'prismjs/components/prism-json';
   import 'prismjs/components/prism-markdown';
